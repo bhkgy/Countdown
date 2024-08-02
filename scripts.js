8a8f320978646c9b9e8bd1a1d15cce060a5d5a7b
kotamun <167137931+bhkgy@users.noreply.github.com>
2024-08-02 11:37:31 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,21 +3,20 @@ function countdownTimer() {
     const now = new Date().getTime();
     const distance = targetDate - now;
 
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    const milliseconds = Math.floor(distance % 1000);
+    if (distance > 0) {
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        const milliseconds = Math.floor(distance % 1000);
 
-    document.getElementById("days").innerText = days;
-    document.getElementById("hours").innerText = hours;
-    document.getElementById("minutes").innerText = minutes;
-    document.getElementById("seconds").innerText = seconds;
-    document.getElementById("milliseconds").innerText = milliseconds;
-
-    if (distance < 0) {
+        document.getElementById("days").innerText = days;
+        document.getElementById("hours").innerText = hours;
+        document.getElementById("minutes").innerText = minutes;
+        document.getElementById("seconds").innerText = seconds;
+        document.getElementById("milliseconds").innerText = milliseconds;
+    } else {
         clearInterval(interval);
-        document.getElementById("countdown").innerText = "EXPIRED";
     }
 }
 
